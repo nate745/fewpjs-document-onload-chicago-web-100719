3d6bb5c54da92f3e40063dc2db53d7ba74f20c97
Nathan Alex <nate745@gmail.com>
2019-11-19 17:25:34 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    insertText();
+});
+
+function insertText() {
+
+    let text = document.getElementById('text')
+    return text.innerHTML = "This is really cool!"
+}
